4beb7410b2bfc89d556f6405785e40d853b792d5
1010290 <s1010290@usls.edu.ph>
2026-03-24 22:58:14 +0800

    Set up routes to user pages

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,25 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.user.homepage');
+});
+
+Route::get('/hotels', function () {
+    return view('pages.user.hotels');
+});
+
+Route::get('/booking', function () {
+    return view('pages.user.booking');
+});
+
+Route::get('/paymentdetails', function () {
+    return view('pages.user.paymentDetails');
+});
+
+Route::get('/paymentverification', function () {
+    return view('pages.user.paymentVerification');
+});
+
+Route::get('/contacts', function () {
+    return view('pages.user.contacts');
 });
