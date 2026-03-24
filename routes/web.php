964db52b980c069a5285adbb167ef8fd5a59091a
Shekina-Tautho <supagwapashikyna@gmail.com>
2026-03-25 05:23:28 +0800

    refactor(routing): remove duplicate controllers and link routes to existing controller

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,11 @@ use App\Http\Controllers\AccountCreationController;
 use App\Http\Controllers\AdminDashboardController;
 use App\Http\Controllers\AdminBookingsController;
 use App\Http\Controllers\AdminUserManagementController;
-use App\Http\Controllers\UserHomepageController;
+use App\Http\Controllers\HomepageController;
 use App\Http\Controllers\UserHotelSearchController;
-use App\Http\Controllers\UserHotelsController;
+use App\Http\Controllers\HotelsController;
 use App\Http\Controllers\UserReservationsController;
-use App\Http\Controllers\UserContactsController;
+use App\Http\Controllers\ContactsController;
 use App\Http\Controllers\UserAccountController;
 
 #Login and Create Account
@@ -23,9 +23,9 @@ Route::get('/AdminBookings', [AdminBookingsController::class, 'index']);
 Route::get('/AdminUserManagement', [AdminUserManagementController::class, 'index']);
 
 #User Pages
-Route::get('/UserHomePage', [UserHomePageController::class, 'index']);
+Route::get('/Homepage', [HomepageController::class, 'index']);
 Route::get('/UserHotelSearch', [UserHotelSearchController::class, 'index']);
-Route::get('/UserHotels', [UserHotelsController::class, 'index']);
+Route::get('/Hotels', [HotelsController::class, 'index']);
 Route::get('/UserReservations', [UserReservationsController::class, 'index']);
-Route::get('/UserContacts', [UserContactsController::class, 'index']);
+Route::get('/Contacts', [ContactsController::class, 'index']);
 Route::get('/UserAccount', [UserAccountController::class, 'index']);
