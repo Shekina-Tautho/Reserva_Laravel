5570401f37e6b231330abe4ea61cc010f9fa4b2d
Shekina-Tautho <supagwapashikyna@gmail.com>
2026-03-25 05:50:31 +0800

    chore(routes): allow page viewing without controller (for presentation)

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,36 @@ Route::get('/AdminBookings', [AdminBookingsController::class, 'index']);
 Route::get('/AdminUserManagement', [AdminUserManagementController::class, 'index']);
 
 #User Pages
-Route::get('/Homepage', [HomepageController::class, 'index']);
+#Route::get('/Homepage', [HomepageController::class, 'index']);
 Route::get('/UserHotelSearch', [UserHotelSearchController::class, 'index']);
-Route::get('/Hotels', [HotelsController::class, 'index']);
+#Route::get('/Hotels', [HotelsController::class, 'index']);
 Route::get('/UserReservations', [UserReservationsController::class, 'index']);
-Route::get('/Contacts', [ContactsController::class, 'index']);
+#Route::get('/Contacts', [ContactsController::class, 'index']);
 Route::get('/UserAccount', [UserAccountController::class, 'index']);
+
+
+
+#pages that don't use controller as of now for presentation purposes
+Route::get('/homepage', function() {
+    return view('pages.user.homepage');
+});
+
+Route::get('/hotels', function() {
+    return view('pages.user.hotels');
+});
+
+Route::get('/booking', function() {
+    return view('pages.user.booking');
+});
+
+Route::get('/paymentdetails', function() {
+    return view('pages.user.paymentDetails');
+});
+
+Route::get('/paymentverification', function() {
+    return view('pages.user.paymentVerification');
+});
+
+Route::get('/contacts', function() {
+    return view('pages.user.contacts');
+});
