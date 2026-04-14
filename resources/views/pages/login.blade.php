b8c334f669925d64c634011434bf912923de4f88
maeerii <147013689+maeerii@users.noreply.github.com>
2026-04-15 02:47:39 +0800

    CSS adjustments login & reg

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -1,36 +1,63 @@
 @extends('layouts.user.content')
 
 @section('content')
-    <h2>Login</h2>
 
-    {{-- Show validation errors --}}
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+<link rel="stylesheet" href="{{ asset('css/login.css') }}">
+
+<div class="main-div">
+
+    <!-- LEFT SECTION -->
+    <div class="d-flex flex-column justify-content-center align-items-center w-50 position-relative">
+        <div class="bg-overlay"></div>
+        <img class="bg-section-img"
+            src="{{ asset('images/reserva-branding.png') }}" 
+        >
+    </div>
+
+    <div class="login-section d-flex justify-content-center align-items-center w-50">
+        <div class="form-login text-center">
+            <h1 class="login-heading mb-4">Welcome Back!</h1>
+
+            @if ($errors->any())
+                <div class="alert alert-danger text-start">
+                    <ul class="mb-0">
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
+            <form action="{{ route('login.post') }}" method="POST">
+                @csrf
+                <input 
+                    type="email" 
+                    name="email" 
+                    placeholder="example@email.com"
+                    class="form-control login-input mb-3"
+                    value="{{ old('email') }}"
+                    required
+                >
+                <input 
+                    type="password" 
+                    name="password" 
+                    placeholder="••••••••"
+                    class="form-control login-input login-password mb-4"
+                    required
+                >
+                <button type="submit" id="login-button" class="w-100 py-2">
+                    Log In
+                </button>
+            </form>
+
+            <p class="register-p">
+                Not a member? 
+                <a href="{{ route('register') }}">Register Now</a>
+            </p>
+
         </div>
-    @endif
+    </div>
 
-    <form action="{{ route('login.post') }}" method="POST">
-        @csrf
-        <div>
-            <label for="email">Email address</label>
-            <input type="email" name="email" id="email" value="{{ old('email') }}" required autofocus>
-        </div>
+</div>
 
-        <div>
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password" required>
-        </div>
-
-        <button type="submit">Log In</button>
-    </form>
-
-    <p>
-        Don’t have an account?
-        <a href="{{ route('register') }}">Register here</a>
-    </p>
 @endsection
--- resources/views/pages/register.blade.php
+++ resources/views/pages/register.blade.php
@@ -1,24 +1,101 @@
 @extends('layouts.user.content')
 
 @section('content')
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-    <form action="{{ route('register.post') }}" method="POST">
-        @csrf
-        <input type="email" name="email" id="email" placeholder="Email address">
-        <input type="password" name="password" id="password" placeholder="Password">
-        <input type="text" name="first_name" id="first_name" placeholder="First Name">
-        <input type="text" name="last_name" id="last_name" placeholder="Last Name">        
-        <input type="text" name="phone_no" id="phone_no" placeholder="Phone Number" pattern="^093[0-9]{9}$">        
-        <button type="submit">Register</button>
-    </form>
 
-    <a href="{{ route('register') }}"></a>
-@endsection 
+<link rel="stylesheet" href="{{ asset('css/signup.css') }}">
+
+<div class="main-div">
+
+    <!-- LEFT SECTION -->
+    <div class="d-flex flex-column justify-content-center align-items-center w-50 position-relative">
+        <div class="bg-overlay"></div>
+        <img class="bg-section-img"
+            src="{{ asset('images/reserva-branding.png') }}" 
+        >
+    </div>
+
+    <div class="login-section d-flex justify-content-center align-items-center w-50">
+        <div class="form-login flex-column">
+            <h1 class="login-heading text-center mx-auto mb-4">Let's Get Started</h1>
+
+            @if ($errors->any())
+                <div class="alert alert-danger text-start">
+                    <ul class="mb-0">
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
+            <form action="{{ route('register.post') }}" method="POST">
+                @csrf
+
+                <input 
+                    type="email" 
+                    name="email" 
+                    placeholder="example@email.com"
+                    class="form-control signup-input mb-3"
+                    value="{{ old('email') }}"
+                    required
+                >
+
+                <div class="d-flex gap-2 mb-3">
+                    <input 
+                        type="text" 
+                        name="first_name" 
+                        placeholder="First Name"
+                        class="form-control signup-input"
+                        value="{{ old('first_name') }}"
+                        required
+                    >
+
+                    <input 
+                        type="text" 
+                        name="last_name" 
+                        placeholder="Last Name"
+                        class="form-control signup-input"
+                        value="{{ old('last_name') }}"
+                        required
+                    >
+                </div>
+
+                <input 
+                    type="password" 
+                    name="password" 
+                    placeholder="••••••••"
+                    class="form-control signup-input mb-3"
+                    required
+                >
+
+                <input 
+                    type="text" 
+                    name="phone_no" 
+                    placeholder="Phone Number"
+                    class="form-control signup-input mb-4"
+                    value="{{ old('phone_no') }}"
+                    pattern="^09[0-9]{9}$"
+                >
+
+                <button type="submit" id="signup-button" class="w-100 py-2">
+                    Sign Up
+                </button>
+            </form>
+
+            <p class="terms mt-3">
+                By signing up, you agree to our 
+                <span class="blue">Terms of Use</span> and 
+                <span class="blue">Privacy Policy</span>.
+            </p>
+
+            <p class="register-p">
+                Already have an account? 
+                <a href="{{ route('login') }}" class="signin-link">Sign in.</a>
+            </p>
+
+        </div>
+    </div>
+
+</div>
+
+@endsection
