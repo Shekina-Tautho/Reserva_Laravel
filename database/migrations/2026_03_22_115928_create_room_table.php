6eab60508ad154a7f19eedaa19e8387a8674af53
1010290 <s1010290@usls.edu.ph>
2026-04-14 12:10:10 +0800

    Filled room seeder

--- database/migrations/2026_03_22_115928_create_room_table.php
+++ database/migrations/2026_03_22_115928_create_room_table.php
@@ -22,6 +22,8 @@ return new class extends Migration
             $table->string('room_type');
             $table->string('capacity');
             $table->string('amenities');
+            $table->string('no_of_beds');
+            $table->double('room_rates');
             $table->string('image_path')->nullable();
             $table->timestamps();
         });
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             EmployeeSeeder::class,
-            HotelSeeder::class
+            HotelSeeder::class,
+            RoomSeeder::class
         ]);
     }
 }
